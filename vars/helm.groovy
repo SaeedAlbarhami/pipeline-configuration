4dd6b60605f77dac0fc4863f1b88b60fdfa5a8eb
SaeedAlbarhami <saeedalbarhami@yahoo.com>
2019-10-30 12:28:22 +0400

    Update helm.groovy

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -8,9 +8,6 @@ def install_upgrade(helm_chart, revision, namespace, env)
        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/SaeedAlbarhami/pipeline-configuration.git']]])
        sh "git status" 
        sh " ls -a "
-       sh "cd helm"
-       sh "pwd"  
-       sh " ls -a "
        sh "helm upgrade --install --force '${helm_chart}' 'helm/${helm_chart}' -f 'helm/${helm_chart}'/values-'${env}'.yaml --set image.tag='${revision}' --namespace='${namespace}'"
        echo "To rollback to the previous revision (helm rollback ${helm_chart} 0)"
        sh "helm history ${helm_chart}"                    
